84c2c309b2d6a698e6e6cd5a5de2387ba18121b0
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-03-20 18:09:31 -0700

    Added bits files

--- bits.c
+++ bits.c
@@ -10,16 +10,14 @@ uint32_t get_bits(uint64_t num, uint32_t start, uint32_t count) {
 
 // Sign extend a number from a specified bit position
 int64_t sign_extend(uint64_t num, uint32_t start) {
-    int32_t signed_num = ((int32_t) num)  >> start;
-    int64_t signed_num_64 = (int64_t) signed_num;
-    return signed_num_64;
-}
-
-// Get a specific bit from a number
-bool get_bit(uint64_t num, uint32_t start) {
     uint32_t distance = 64 - start;
     int64_t imm;
     imm = num << distance;
     imm = imm >> distance;
     return imm;
+}
+
+// Get a specific bit from a number
+bool get_bit(uint64_t num, uint32_t bit) {
+    return (num >> bit) & 1U;
 }
--- bits.h
+++ bits.h
@@ -3,4 +3,4 @@
 
 uint32_t get_bits(uint64_t num, uint32_t start, uint32_t count);
 int64_t sign_extend(uint64_t num, uint32_t start);
-bool get_bit(uint64_t num, uint32_t which);
+bool get_bit(uint64_t num, uint32_t bit);
