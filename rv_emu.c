b5e15dc817b9654b57863a2b4e662d1fa89295ae
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-03-23 13:06:24 -0700

    Extended implementation to support BEQ, SLLW, SRAW

--- rv_emu.c
+++ rv_emu.c
@@ -94,11 +94,21 @@ void emu_r_type(struct rv_state_st *rsp, uint32_t iw) {
     rsp->pc += 4; // Next instruction
 }
 
-void emu_jalr(struct rv_state_st *rsp, uint32_t iw) {
+void emu_jalr (struct rv_state_st *rsp, uint32_t iw) {
     uint32_t rs1 = (iw >> 15) & 0b1111;  // Will be ra (aka x1)
     uint64_t val = rsp->regs[rs1];  // Value of regs[1]
 
     rsp->pc = val;  // PC = return address
+}
+
+void emu_jal (struct rv_state_st *rsp, uint32_t iw) {
+    uint32_t rd = rv_get_rd(iw);
+    uint32_t immu21 = (get_bit(iw, 31) << 20) | (get_bits(iw, 12, 8) << 12) | (get_bit(iw, 20) << 11) | (get_bits(iw, 21, 10) << 1);
+    int64_t imm = sign_extend(immu21, 20);
+    if(rd != 0){
+        rsp->regs[rd] = rsp->pc + 4;
+    }
+    rsp->pc += imm;
 }
 
 void emu_i_type (struct rv_state_st *rsp, uint32_t iw) {
@@ -107,12 +117,11 @@ void emu_i_type (struct rv_state_st *rsp, uint32_t iw) {
     uint32_t rs2 = rv_get_rs2(iw);
     uint32_t funct3 = rv_get_funct3(iw);
     uint32_t funct7 = rv_get_funct7(iw);
-
+    uint32_t immu32 = get_bits(iw, 20, 12);
+    int64_t imm = sign_extend(immu32, 11);
     if (funct3 == 0b101 && funct7 == 0b0000000) {   //SRLI
         rsp->regs[rd] = rsp->regs[rs1] >> rs2;
     } else if (funct3 == 0b000){    //ADDI
-        uint64_t immu64 = get_bits(iw, 20, 11);
-        int64_t imm = sign_extend(immu64, 11);
         if(rs1==0){
             rsp->regs[rd] = imm;
         } else {
@@ -123,35 +132,101 @@ void emu_i_type (struct rv_state_st *rsp, uint32_t iw) {
     }
     rsp->pc += 4; // Next instruction
 }
+// 0b0000000 
 
-void emu_b_type(struct rv_state_st *rsp, uint32_t iw){
+void emu_64i_type (struct rv_state_st *rsp, uint32_t iw) {
+    uint32_t rd = rv_get_rd(iw);
+    uint32_t funct3 = rv_get_funct3(iw);
+    uint32_t rs1 = rv_get_rs1(iw);
+    uint32_t rs2 = rv_get_rs2(iw);
+    uint32_t funct7 = rv_get_funct7(iw);
+    uint32_t shamt = get_bits(rsp->regs[rs2], 0, 5);
+    if (funct3 == 0b001 && funct7 == 0b0000000) { //SLLW
+        int32_t res = get_bits(rsp->regs[rs1], 0, 32) << shamt;
+        rsp->regs[rd] = res;
+    } else if (funct3 == 0b101 && funct7 == 0b0100000) { //SRAW
+        int32_t res = ((int32_t)rsp->regs[rs1] >> shamt);
+        rsp->regs[rd] = res;
+    } else {
+        unsupported("64I-type funct3", funct3);
+    }
+    rsp->pc += 4; // Next instruction
+     
+}
+
+void emu_b_type (struct rv_state_st *rsp, uint32_t iw) {
     uint32_t rs1 = rv_get_rs1(iw);
     uint32_t rs2 = rv_get_rs2(iw);
     uint32_t funct3 = rv_get_funct3(iw);
 
-    uint64_t immu64 = (get_bit(iw, 31) << 12) | (get_bits(iw, 25, 6) << 5) | (get_bit(iw, 7) << 11) | (get_bits(iw, 8, 4) << 1);
-    int64_t imm = sign_extend(immu64, 12);  // Sign extend to 64 bits
+    uint32_t immu32 = (get_bit(iw, 31) << 12) | (get_bits(iw, 25, 6) << 5) | (get_bit(iw, 7) << 11) | (get_bits(iw, 8, 4) << 1);
+    int64_t imm = sign_extend(immu32, 12);  // Sign extend to 64 bits
 
     int64_t rs1_val = (int64_t) rsp->regs[rs1];
     int64_t rs2_val = (int64_t) rsp->regs[rs2];
     
-    if ((funct3 == 0b100 && rs1_val < rs2_val) ||  //BLT
-        (funct3 == 0b001 && rs1_val != rs2_val) || //BNE
-        (funct3 == 0b101 && rs1_val >= rs2_val)) { //BGE
+    if ((funct3 == 0b000 && (rs1_val == rs2_val)) || //BEQ
+        (funct3 == 0b100 && (rs1_val < rs2_val))  || //BLT
+        (funct3 == 0b001 && (rs1_val != rs2_val)) || //BNE
+        (funct3 == 0b101 && (rs1_val >= rs2_val))) { //BGE
             rsp->pc += imm;
-    } else if (funct3 == 0b100 || funct3 == 0b001 || funct3 == 0b101) {
+    } else if (funct3 == 0b000 || funct3 == 0b100 || funct3 == 0b001 || funct3 == 0b101) {
         rsp->pc += 4; // Next instruction
     } else {
         unsupported("B-type funct3", funct3);
     }
 }
 
-static void rv_one(struct rv_state_st *rsp) {
+void emu_s_type (struct rv_state_st *rsp, uint32_t iw){
+    uint32_t rs1 = rv_get_rs1(iw);
+    uint32_t rs2 = rv_get_rs2(iw);
+    uint32_t funct3 = rv_get_funct3(iw);
+
+    uint32_t immu32 = get_bits(iw, 25, 7) << 5 | get_bits(iw, 7, 5);
+    int64_t imm = sign_extend(immu32, 11);
+    uint64_t target_addr = rsp->regs[rs1]+imm;
+    
+    if (funct3 == 0b011) { //SD
+        *((uint64_t *) target_addr) = rsp->regs[rs2];
+    } else if (funct3 == 0b000) { //SB
+        *((uint8_t *) target_addr) = rsp->regs[rs2];
+    } else if (funct3 == 0b010) { //SW
+        *((uint32_t *) target_addr) = rsp->regs[rs2];
+    } else {
+        unsupported("S-type funct3", funct3);
+    }
+    rsp->pc += 4; // Next instruction
+
+}
+
+void emu_load (struct rv_state_st *rsp, uint32_t iw) {
+    uint32_t rd = rv_get_rd(iw);
+    uint32_t rs1 = rv_get_rs1(iw);
+    uint32_t funct3 = rv_get_funct3(iw);
+    uint32_t immu32 = get_bits(iw, 20, 12);
+    int64_t imm = sign_extend(immu32, 11);
+    uint64_t target_addr = rsp->regs[rs1]+imm;
+    if (funct3 == 0b011) { //LD
+        uint64_t value = *((uint64_t *) target_addr);
+        rsp->regs[rd] = value;
+    } else if (funct3 == 0b010) { //LW   
+        uint32_t value = *((uint32_t *) target_addr);
+        rsp->regs[rd] = value;
+    } else if (funct3 == 0b000) { //LB
+        uint64_t target_addr = rsp->regs[rs1]+imm;
+        uint8_t value = *((uint8_t *) target_addr);
+        rsp->regs[rd] = value;
+    } else{
+        unsupported("I-type funct3", funct3);
+    }
+    rsp->pc += 4; // Next instruction
+}
+
+static void rv_one (struct rv_state_st *rsp) {
     uint32_t iw  = *((uint32_t*) rsp->pc);
     //iw = cache_lookup(&rsp->i_cache, (uint64_t) rsp->pc);
 
     uint32_t opcode = iw & 0b1111111;
-
 #if DEBUG
     printf("iw: %08x\n", iw);
 #endif
@@ -165,11 +240,23 @@ static void rv_one(struct rv_state_st *rsp) {
             // JALR (aka RET) is a variant of I-type instructions
             emu_jalr(rsp, iw);
             break;
+        case FMT_J:
+            emu_jal(rsp, iw);
+            break;
         case FMT_I_ARITH:
             emu_i_type(rsp, iw);
             break;
+        case FMT_64I_ARITH:
+            emu_64i_type(rsp, iw);
+            break;
+        case FMT_I_LOAD:
+            emu_load(rsp, iw);
+            break;
         case FMT_B:
             emu_b_type(rsp, iw);
+            break;
+        case FMT_S:
+            emu_s_type(rsp, iw);
             break;
         default:
             unsupported("Unknown opcode: ", opcode);
